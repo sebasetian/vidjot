bf6e44d79b0122f38daa908e322bf7d87cfd1832
Shih-Yao_Lin <sebasetian123@gmail.com>
2018-04-14 16:53:50 -0700

    add routers

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = express();
 
 const port = 5000;
 
+// Index Route
+app.get('/',(req,res) => {
+    res.send('Index');
+});
+
+app.get('/about',(req,res) => {
+    res.send('About');
+});
+
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+    console.log(`Server started on port ${port} yaasa`);
 });
