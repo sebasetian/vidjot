53b0241e4f6f00c21fcc3ee3ee52c1a7dfc4e276
Shih-Yao_Lin <sebasetian123@gmail.com>
2018-04-14 20:39:48 -0700

    add mongodb database

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express =  require('express');
+const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev')
+    .then(() => console.log(`MongoDB connected......`))
+    .catch((err) => console.log(err));
+
+// Load Idea Model
+require('./models/ideas');
+const Idea = mongoose.model('ideas');
+
+//handlebar middleware
+app.engine('handlebars',
+    exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
 
 const port = 5000;
 
 // Index Route
 app.get('/',(req,res) => {
-    res.send('Index');
+    const title = 'Welcome';
+    res.render('Index',
+        {title: title}
+        );
 });
 
 app.get('/about',(req,res) => {
-    res.send('About');
+    res.render('about');
 });
 
 app.listen(port, () => {
